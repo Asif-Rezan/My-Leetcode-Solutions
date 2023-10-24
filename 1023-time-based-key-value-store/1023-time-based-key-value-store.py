433fc11775d267553f740be35284e6b4b4dd9ab2
Md. Shahriar Asif Rezan <asifrezan@gmail.com>
2023-10-24 20:15:37 +0600

    Time: 635 ms (28.67%), Space: 75 MB (35.49%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -0,0 +1,31 @@
+class TimeMap(object):
+
+    def __init__(self):
+        self.store ={}
+        
+
+    def set(self, key, value, timestamp):
+        if key not in self.store:
+            self.store[key] = []
+        
+        self.store[key].append([value, timestamp])
+
+        
+    def get(self, key, timestamp):
+        res = ""
+        values = self.store.get(key, [])
+
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            m = (l+r) // 2
+
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            
+            else:
+                r = m - 1
+    
+        return res
+
