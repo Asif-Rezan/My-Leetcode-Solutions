f14af61b027dc7815af1f83847210a259b3aa16d
Md. Shahriar Asif Rezan <asifrezan@gmail.com>
2026-01-28 16:36:47 +0600

    Time: 3 ms (25%), Space: 12.4 MB (86.9%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
+
         prefix_sum = nums[0]
         for i in range(1, len(nums)): 
             if nums[i] == nums[i-1] + 1 :
@@ -11,11 +13,11 @@ class Solution(object):
 
             else:
                 break
-        
-        num_set = set(nums)
-        x = prefix_sum
 
-        while x in num_set: 
-            x += 1
 
-        return x
+        nums.sort()
+
+        for i in range(n):
+            if prefix_sum == nums[i]:
+                prefix_sum += 1
+        return prefix_sum
